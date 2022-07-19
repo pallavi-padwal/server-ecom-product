174bde86e754021a96fb04f2f5660a78ce4e8061
Rohan Ambhore <pallavi.padwal147@gmail.com>
2022-07-19 23:11:56 +0530

    asd

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
+var data = require('./app.json');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/products', function(request, response) {
+    response.json(data);
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
