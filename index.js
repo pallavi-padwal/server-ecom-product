dbda95d3df7941c89dfe329e21a9c39873e8140e
Rohan Ambhore <pallavi.padwal147@gmail.com>
2022-07-20 00:57:08 +0530

    asd

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var data = require('./app.json');
+var cors = require('cors');
 var app = express()
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
